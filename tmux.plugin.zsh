45fa991f70274af3896683172c9be267141050c1
Orien Vandenbergh <orien.vandenbergh@defpoint.com>
2019-08-21 11:26:10 -0600

    This is how I think auto-tmux should operate

--- tmux.plugin.zsh
+++ tmux.plugin.zsh
@@ -9,104 +9,45 @@ alias tksv='tmux kill-server'
 alias tkss='tmux kill-session -t'
 
 # Only run if tmux is actually installed
-if which tmux &> /dev/null
-	then
-	# Helper function to generate a sessionname
-	function _zsh_tmux_session_name_gen() {
-		[[ -n "$ZSH_TMUX_GENERATE_SESSIONNAME" ]] || exit
-		[[ -n "$ZSH_TMUX_GENERATE_SESSIONNAME_LENGTH" ]] || ZSH_TMUX_GENERATE_SESSIONNAME_LENGTH=6
-		local tmux_sessionname=`date "+%Y%m%d%H%M%S%N$$" | shasum -a 512224`
-		echo ${tmux_sessionname[0,$ZSH_TMUX_GENERATE_SESSIONNAME_LENGTH]}
-	}
-	# Configuration variables
-	#
-	# Automatically start tmux
-	[[ -n "$ZSH_TMUX_AUTOSTART" ]] || ZSH_TMUX_AUTOSTART=false
-	# Only autostart once. If set to false, tmux will attempt to
-	# autostart every time your zsh configs are reloaded.
-	[[ -n "$ZSH_TMUX_AUTOSTART_ONCE" ]] || ZSH_TMUX_AUTOSTART_ONCE=true
-	# Automatically connect to a previous session if it exists
-	[[ -n "$ZSH_TMUX_AUTOCONNECT" ]] || ZSH_TMUX_AUTOCONNECT=true
-	# Automatically close the terminal when tmux exits
-	[[ -n "$ZSH_TMUX_AUTOQUIT" ]] || ZSH_TMUX_AUTOQUIT=$ZSH_TMUX_AUTOSTART
-	# Set term to screen or screen-256color based on current terminal support
-	[[ -n "$ZSH_TMUX_FIXTERM" ]] || ZSH_TMUX_FIXTERM=true
-	# Set '-CC' option for iTerm2 tmux integration
-	[[ -n "$ZSH_TMUX_ITERM2" ]] || ZSH_TMUX_ITERM2=false
-	# If a session name isn't specified, use no session name
-	[[ -n "$ZSH_TMUX_GENERATE_SESSIONNAME" ]] || ZSH_TMUX_GENERATE_SESSIONNAME=false
-	# The TERM to use for non-256 color terminals.
-	# Tmux states this should be screen, but you may need to change it on
-	# systems without the proper terminfo
-	[[ -n "$ZSH_TMUX_FIXTERM_WITHOUT_256COLOR" ]] || ZSH_TMUX_FIXTERM_WITHOUT_256COLOR="screen"
-	# The TERM to use for 256 color terminals.
-	# Tmux states this should be screen-256color, but you may need to change it on
-	# systems without the proper terminfo
-	[[ -n "$ZSH_TMUX_FIXTERM_WITH_256COLOR" ]] || ZSH_TMUX_FIXTERM_WITH_256COLOR="screen-256color"
+if which tmux &> /dev/null; then
+  # Wrapper function for tmux.
+  function _zsh_tmux_plugin_run()
+  {
+    local long_sessionname=`date "+%Y%m%d%H%M%S%N$$" | shasum -a 512224`
+    local tmux_sessionname=${long_sessionname[0,6]}
+    local tmux_sessions=$(\tmux ls >/dev/null 2>&1 && echo true || echo false)
+    # Some people seem to like tmux/iterm2 integration, maybe one day that will be me?
+    local tmux_iterm2=$( [ -n "$ITERM_PROFILE" ] && echo '-CC' )
+    # But not today...
+    tmux_iterm2=''
 
+    # We have other arguments, just run them
+    if [[ -n "$@" ]]; then
+      \tmux $@
+    # Try to connect to our 'zsh_shared' session, under a new session
+    elif [[ $(\tmux ls 2>/dev/null | grep -q 'zsh_shared' && echo true || echo false) == "true" ]]; then
+      \tmux $tmux_iterm2 new-session -s $tmux_sessionname -t zsh_shared && exit
+    # Create a new zsh_shared session
+    else
+      \tmux $tmux_iterm2 new-session -s zsh_shared && exit
+    fi
+  }
 
-	# Get the absolute path to the current directory
-	local zsh_tmux_plugin_path="$(cd "$(dirname "$0")" && pwd)"
+  # Use the completions for tmux for our function
+  compdef _tmux _zsh_tmux_plugin_run
 
-	# Determine if the terminal supports 256 colors
-	if [[ `tput colors` == "256" ]]
-	then
-		export ZSH_TMUX_TERM=$ZSH_TMUX_FIXTERM_WITH_256COLOR
-	else
-		export ZSH_TMUX_TERM=$ZSH_TMUX_FIXTERM_WITHOUT_256COLOR
-	fi
+  # Alias tmux to our wrapper function.
+  alias tmux=_zsh_tmux_plugin_run
 
-	# Set the correct local config file to use.
-    if [[ "$ZSH_TMUX_ITERM2" == "false" ]] && [[ -f $HOME/.tmux.conf || -h $HOME/.tmux.conf ]]
-	then
-		#use this when they have a ~/.tmux.conf
-		export _ZSH_TMUX_FIXED_CONFIG="$zsh_tmux_plugin_path/tmux.extra.conf"
-	else
-		#use this when they don't have a ~/.tmux.conf
-		export _ZSH_TMUX_FIXED_CONFIG="$zsh_tmux_plugin_path/tmux.only.conf"
-	fi
-
-	# Wrapper function for tmux.
-	function _zsh_tmux_plugin_run()
-	{
-		# Configure the sessionname
-		if [[ "$ZSH_TMUX_GENERATE_SESSIONNAME" == "true" ]]; then
-			ZSH_TMUX_SESSIONNAME="$(_zsh_tmux_session_name_gen)"
-		fi
-		[[ -n "$ZSH_TMUX_SESSIONNAME" ]] && ZSH_TMUX_NAMEARG="-s$ZSH_TMUX_SESSIONNAME" || ZSH_TMUX_NAMEARG=""
-
-		# We have other arguments, just run them
-		if [[ -n "$@" ]]
-		then
-			\tmux $@
-		# Try to connect to an existing session.
-		elif [[ "$ZSH_TMUX_AUTOCONNECT" == "true" ]]
-		then
-			\tmux `[[ "$ZSH_TMUX_ITERM2" == "true" ]] && echo '-CC '` attach || \tmux `[[ "$ZSH_TMUX_ITERM2" == "true" ]] && echo '-CC '` `[[ "$ZSH_TMUX_FIXTERM" == "true" ]] && echo '-f '$_ZSH_TMUX_FIXED_CONFIG` new-session $ZSH_TMUX_NAMEARG
-			[[ "$ZSH_TMUX_AUTOQUIT" == "true" ]] && exit
-		# Just run tmux, fixing the TERM variable if requested.
-		else
-			\tmux `[[ "$ZSH_TMUX_ITERM2" == "true" ]] && echo '-CC '` `[[ "$ZSH_TMUX_FIXTERM" == "true" ]] && echo '-f '$_ZSH_TMUX_FIXED_CONFIG` new-session $ZSH_TMUX_NAMEARG
-			[[ "$ZSH_TMUX_AUTOQUIT" == "true" ]] && exit
-		fi
-	}
-
-	# Use the completions for tmux for our function
-	compdef _tmux _zsh_tmux_plugin_run
-
-	# Alias tmux to our wrapper function.
-	alias tmux=_zsh_tmux_plugin_run
-
-	# Autostart if not already in tmux and enabled.
-	if [[ ! -n "$TMUX" && "$ZSH_TMUX_AUTOSTART" == "true" ]]
-	then
-		# Actually don't autostart if we already did and multiple autostarts are disabled.
-		if [[ "$ZSH_TMUX_AUTOSTART_ONCE" == "false" || "$ZSH_TMUX_AUTOSTARTED" != "true" ]]
-		then
-			export ZSH_TMUX_AUTOSTARTED=true
-			_zsh_tmux_plugin_run
-		fi
-	fi
+  # Autostart if not already in tmux and enabled.
+  if [[ ! -n "$TMUX" ]]; then
+    # Actually don't autostart if we already did and multiple autostarts are disabled.
+    if [[ "$ZSH_TMUX_AUTOSTARTED" != "true" ]]
+    then
+      export ZSH_TMUX_AUTOSTARTED=true
+      _zsh_tmux_plugin_run
+    fi
+  fi
 else
-	print "zsh tmux plugin: tmux not found. Please install tmux before using this plugin."
+  print "zsh tmux plugin: tmux not found. Please install tmux before using this plugin."
 fi
